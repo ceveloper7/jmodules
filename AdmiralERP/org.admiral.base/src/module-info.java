0ab3d96fbf3cd7ba623e0eb8715c91a6823abd7b
ceveloper7 <cevabarcvilla@gmail.com>
2025-01-07 19:52:13 -0300

    Java Core Vol I - Chapter 12 - Java Platform Module System - Basic module: org.admiral.base

---
+++ AdmiralERP/org.admiral.base/src/module-info.java
@@ -0,0 +1,2 @@
+module org.admiral.base {
+}
---
+++ AdmiralERP/org.admiral.base/src/org/admiral/base/Admiral.java
@@ -0,0 +1,7 @@
+package org.admiral.base;
+
+public class Admiral {
+    public static void main(String[] args) {
+        System.out.println("Iniciando el Sistema ERP Admiral 1.0");
+    }
+}
