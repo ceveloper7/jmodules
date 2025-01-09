2f2f1ebd7cce1013a2db97c114bf91caf5171a4a
ceveloper7 <cevabarcvilla@gmail.com>
2025-01-09 19:12:45 -0300

    Java Core Vol 1 - Chapter 12 - Java Platform Module System - Service Loading

--- AdmiralERP/com.admiral.greetsvc/src/module-info.java
+++ AdmiralERP/com.admiral.greetsvc/src/module-info.java
@@ -1,5 +1,6 @@
 module com.admiral.greetsvc {
     exports com.admiral.greetsvc;
+    // Declaramos el servicio GreeterService y las clases que implementan la interface
     provides com.admiral.greetsvc.GreeterService with
             com.admiral.greetsvc.internal.FrenchGreeter,
             com.admiral.greetsvc.internal.GermanGreeterFactory;
--- AdmiralERP/com.admiral.usesvcservice/src/module-info.java
+++ AdmiralERP/com.admiral.usesvcservice/src/module-info.java
@@ -1,4 +1,8 @@
 module com.admiral.usesvcservice {
     requires com.admiral.greetsvc;
+    // Consumimos el servicio exportado por el modulo com.admiral.greetsvc
+    // como resultado de las clausulas provides en el modulo com.admiral.greetsvc y
+    // uses en com.admiral.usesvcservice, este modulo ahora que consume el serlvicio GreeterService
+    // se le permite el acceso a las clases de implementacion del GreeterService
     uses com.admiral.greetsvc.GreeterService;
 }
